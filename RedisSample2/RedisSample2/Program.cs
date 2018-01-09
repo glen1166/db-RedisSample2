484c8ea41bba52e7b2fba8cea52a19432dd4fce5
Guangtao Zhao <zhaoguangtao@outlook.com>
2018-01-09 16:33:07 +0800

    Initial commit.

---
+++ RedisSample2/RedisSample2/Program.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisSample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+            IDatabase db = redis.GetDatabase();
+            if (db.StringSet("testKey", "testValue"))
+            {
+                var val = db.StringGet("testKey");
+
+                Console.WriteLine(val);
+            }
+        }
+    }
+}
