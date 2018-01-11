0f5704fb5c3beb76108e5cd81c0b924990acb876
Guangtao Zhao <zhaoguangtao@outlook.com>
2018-01-11 20:04:18 +0800

    Integer

--- RedisSample2/RedisSample2/Program.cs
+++ RedisSample2/RedisSample2/Program.cs
@@ -13,50 +13,43 @@ namespace RedisSample2
         {
             var redis = RedisStore.RedisCache;
 
-            var key = "testKey";
-
-            if (redis.StringSet(key, "testValue"))
+            var number = 101;
+            var intKey = "intKey";
+            if (redis.StringSet(intKey, number))
             {
-                var val = redis.StringGet(key);
+                //redis incr command
+                var result = redis.StringIncrement(intKey); //after operation Our int number is now 102
+                Console.WriteLine(result);
 
-                //output - StringGet(testKey) value is testValue
-                Console.WriteLine("StringGet({0}) value is {1}", key, val);
+                //incrby command
+                var newNumber = redis.StringIncrement(intKey, 100); // we now have incremented by 100, thus the new number is 202
+                Console.WriteLine(newNumber);
 
-                var v1 = redis.StringGetSet(key, "testValue2");
+                redis.KeyDelete("zeroValueKey");
+                //by default redis stores a value of zero if no value is provided           
+                var zeroValue = (int)redis.StringGet("zeroValueKey");
+                Console.WriteLine(zeroValue);
 
-                //output - StringGetSet(testKey) testValue == testValue
-                Console.WriteLine("StringGetSet({0}) {1} == {2}", key, val, v1);
+                var someValue = (int)redis.StringIncrement("zeroValueKey"); //someValue is now 1 since it was incremented
+                Console.WriteLine(someValue);
 
-                val = redis.StringGet(key);
+                //decr command
+                redis.StringDecrement("zeroValueKey");
+                someValue = (int)redis.StringGet("zeroValueKey"); //now someValue is back to 0   
+                Console.WriteLine(someValue);
 
-                //output - StringGet(testKey) value is testValue2
-                Console.WriteLine("StringGet({0}) value is {1}", key, val);
+                //decrby command
+                someValue = (int)redis.StringDecrement("zeroValueKey", 99); // now someValue is -99   
+                Console.WriteLine(someValue);
 
-                //using SETNX 
-                //code never goes into if since key already exist
-                if (redis.StringSet(key, "someValue", TimeSpan.MaxValue, When.NotExists))
-                {
-                    val = redis.StringGet(key);
-                    Console.WriteLine("StringGet({0}) value is {1}", key, val);
-                }
-                else
-                {
-                    //always goes here
-                    Console.WriteLine("Value already exist");
-                }
+                //append command
+                redis.StringAppend("zeroValueKey", 1);
+                someValue = (int)redis.StringGet("zeroValueKey"); //"Our zeroValueKey number is now -991   
+                Console.WriteLine(someValue);
 
-                var key2 = key + "1";
-                if (redis.StringSet(key2, "someValue", TimeSpan.MaxValue, When.NotExists))
-                {
-                    val = redis.StringGet(key2);
-                    //output - StringGet(testKey2) value is someValue", key2, val
-                    Console.WriteLine("StringGet({0}) value is {1}", key2, val);
-                }
-                else
-                {
-                    //never goes here
-                    Console.WriteLine("Value already exist");
-                }
+                redis.StringSet("floatValue", 1.1);
+                var floatValue = (float)redis.StringIncrement("floatValue", 0.1); //fload value is now 1.2   
+                Console.WriteLine(floatValue);
             }
         }
     }
